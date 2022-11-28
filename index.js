345ea3d9ba23bf08aa400d42aac7e7051b4e8e75
Kiko <66453399+Lushifer666@users.noreply.github.com>
2022-11-28 22:08:18 +0800

    Working Calcu

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 let display = document.getElementById('calculations');
 
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map( button => {
+    button.addEventListener('click',(e) => {
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText = '';
+                break;
+            case '=':
+                display.innerText = eval(display.innerText);
+                break;
+            default:
+            display.innerText += e.target.innerText;
+            console.log(e.target.innerText);
+        }
+    });
+});
